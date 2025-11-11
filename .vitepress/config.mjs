bf8f19f625520c6806dc8b29295dd8fa096e52f6
MGX <3105821798@qq.com>
2025-11-11 21:35:37 +0800

    提交readme

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,8 +1,8 @@
 /*
  * @Author: 中二的开心愚者 3105821798@qq.com
  * @Date: 2025-11-11 21:13:49
- * @LastEditors: 中二的开心愚者 3105821798@qq.com
- * @LastEditTime: 2025-11-11 21:25:40
+ * @LastEditors: MGX 3105821798@qq.com
+ * @LastEditTime: 2025-11-11 21:33:46
  * @FilePath: \vitepress\docsDemo\.vitepress\config.mjs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
